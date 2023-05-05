915c38dbf3c1fa6bcf9edc80c87062211fda3541
SnowGuest <1622583480@qq.com>
2023-05-05 13:51:17 +0800

    first commit

---
+++ src/Button/src/Button.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="mc-button">
+        <slot></slot>
+    </button>
+</template>
+
+<script lang="ts" setup>
+export interface ButtonProps {
+
+}
+defineProps<ButtonProps>()
+</script>
+<style  scoped>
+.mc-button {}
+</style>
